111c18ac5735d967a569e1cae87a083c455177b3
Emiirck <emirck.candelo@tecnologicoliceocristiano.edu.ec>
2024-08-01 23:56:41 -0500

    Unidimensional y Bidimensional

---
+++ Unidimensionales-vectores.py
@@ -0,0 +1,33 @@
+
+#Declarar una lista unidimensional de frutas
+
+frutas = ["Pera", "Manzana", "Piña", "Fresa"]
+
+print(frutas)
+
+frutas.append("Melon")
+
+print(frutas)
+
+#Acceder elemento de la lista
+
+print(frutas[4])
+
+frutas[4] = "Arandano"
+
+print(frutas[4])
+
+#Ejercicio Ordenamiento
+
+digitos = [5, 2, 1, 0, 3, 6, 7, 8, 9]
+print(digitos)
+
+digitos.sort()
+print(digitos)
+
+digitoscero = 5*[0]
+print(digitoscero)
+
+#Ejercicio de tamaño de la lista
+
+print("Cantidad de frutas ingresadas: ", len(frutas))
